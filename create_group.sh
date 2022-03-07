ed682bbf3389421601405df892aab13bd558ff92
Tom Fabler <imagination.fault@gmail.com>
2022-03-07 13:46:29 +0200

    azure keygen is no good; switching to ssh-keygen

--- create_group.sh
+++ create_group.sh
@@ -4,15 +4,6 @@
 source ./include/vars.sh
 
 az group create -l $LOCATION -n $RESOURCE_GRP
-az sshkey create --name $KEY_NAME --resource-group $RESOURCE_GRP 2>./key_stderr
 
-key_generated="$(grep -w Private key_stderr | awk -F'"' '{ print $(NF-1) }')"
-if [ -n $key_generated ]; then
-  mkdir $KEY_DIR
-  mv ${key_generated} ${KEY_DIR}/${KEY_NAME}
-  mv ${key_generated}.pub ${KEY_DIR}/${KEY_NAME}.pub
-  chmod 0600 ${KEY_DIR}/*
-else
-  echo "ERROR: no key in output"
-  cat key_stderr
-fi
+mkdir -p $KEY_DIR
+ssh-keygen -t rsa -b 4096 -C "nobody@nowhere.invalid" -q -N "" -f ${KEY_DIR}/${KEY_NAME}
