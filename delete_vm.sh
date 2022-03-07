adefed8fbc67c4a46b969b678ddfdb26d31b494e
Tom Fabler <imagination.fault@gmail.com>
2022-03-07 18:38:37 +0200

    fix vm deletion untracked resources

--- delete_vm.sh
+++ delete_vm.sh
@@ -6,3 +6,4 @@ source ./include/vars.sh
 az vm delete -g $RESOURCE_GRP -n $VM_NAME --yes
 az network nsg delete -g $RESOURCE_GRP -n ${VM_NAME}NSG
 az network public-ip delete -g $RESOURCE_GRP -n ${VM_NAME}PublicIP
+az network vnet delete -g $RESOURCE_GRP -n ${VM_NAME}VNET
